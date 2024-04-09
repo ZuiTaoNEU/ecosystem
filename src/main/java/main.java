5f832e9ae8bcf09afc6b073982b91b20c5ba9ffd
Zui Tao <tao.zui@northeastern.edu>
2024-04-09 13:13:31 +0800

    Add an sample to get info

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,15 @@
+import model.*;
+
 public class main {
     public static void main(String[] args){
         System.out.println("Hello");
+
+        /** Sample to get information **/
+        // Want to check flight's passenger's number.
+        // Search 'Flight' from Database according to its primary key (flight code).
+        // While the database is not connected now, use a dummy flight.
+        Flight flightCA1929 = new Flight("CA1929", 0, "CA982",
+                        999.92, 103, "JFK019", "PVG002", "19");
+        System.out.println("Check flight's passenger's number:" + flightCA1929.getPassengerNo());
     }
 }
